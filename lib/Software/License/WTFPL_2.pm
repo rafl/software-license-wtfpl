88a39911702097d6ab35fa5f0ecca11025c13c48
Florian Ragwitz <rafl@debian.org>
2010-05-07 04:00:15 +0200

    That paragraph was intented to be verbatim.

--- lib/Software/License/WTFPL_2.pm
+++ lib/Software/License/WTFPL_2.pm
@@ -44,11 +44,11 @@ such, it only covers copying, distribution and modification. If you
 want to add a no warranty clause for a program, you may use the
 following wording in your source code:
 
-/* This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details. */
+  /* This program is free software. It comes without any warranty, to
+   * the extent permitted by applicable law. You can redistribute it
+   * and/or modify it under the terms of the Do What The Fuck You Want
+   * To Public License, Version 2, as published by Sam Hocevar. See
+   * http://sam.zoy.org/wtfpl/COPYING for more details. */
 
 = Isn’t this license basically public domain?
 
